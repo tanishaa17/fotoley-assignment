bf28158df4220ceb8e4f3041591fbcc093e731d1
tanishaa17 <krati1701@gmail.com>
2023-06-03 20:56:31 +0530

    created basic structure

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { useState } from 'react';
 
-export const Home = () => {
-    return (
-        <div>Home</div>
-    )
+
+
+interface ImageDetails {
+    image: string;
+    details: string;
 }
+export const Home = () => {
+
+    const [currrIndex, setCurrIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [isPaused, setIsPaused] = useState(false)
+    const detail = "Details"
+    const img: ImageDetails[] = [
+        { image: '/assets/images/download.jpg', details: detail },
+        { image: '/assets/images/download (1).jpg', details: detail },
+        { image: '/assets/images/download (2).jpg', details: detail },
+        { image: '/assets/images/download (3).jpg', details: detail },
+        { image: '/assets/images/download (4).jpg', details: detail },
+        { image: '/assets/images/images.jpg', details: detail },
+        { image: '/assets/images/images (1).jpg', details: detail },
+        { image: '/assets/images/images (2).jpg', details: detail },
+        { image: '/assets/images/images (3).jpg', details: detail },
+        { image: '/assets/images/images (4).jpg', details: detail },
+    ]
+    return (
+        <div>
+            <Card sx={{ display: 'flex' }}>
+                <CardMedia
+                    component="img"
+                    sx={{ width: 800 }}
+                    image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+                    alt="Live from space album cover"
+                />
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <CardContent sx={{ flex: '1 0 auto' }}>
+                        <Typography component="div" variant="h5">
+                            Detailed Info
+                        </Typography>
+                        <Typography variant="subtitle1" color="text.secondary" component="div" textAlign="left">
+                            {detail}
+                        </Typography>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+
+                        <IconButton aria-label="play/pause">
+                            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                        </IconButton>
+                        <IconButton aria-label="play/pause">
+                            <PauseIcon sx={{ height: 38, width: 38 }} />
+                        </IconButton>
+
+                    </Box>
+                </Box>
+
+            </Card>
+            <IconButton aria-label="play/pause">
+                <NavigateBeforeIcon sx={{ height: 38, width: 38, }} />
+            </IconButton>
+            <IconButton aria-label="play/pause">
+                <NavigateNextIcon sx={{ height: 38, width: 38, }} />
+            </IconButton>
+
+        </div>
+    );
+}
+
+
