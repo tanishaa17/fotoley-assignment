5ea75918e10990ef139c367e3a4e49dd4e0506aa
tanishaa17 <krati1701@gmail.com>
2023-06-04 03:03:19 +0530

    added styling

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -20,8 +20,7 @@ export const Home = () => {
     const [currrIndex, setCurrIndex] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false)
     // const [isPaused, setIsPaused] = useState(false)
-    const [activeImg, setActiveImg] = useState<ImageDetails[]>([]);
-    const detail = "Details"
+    const detail = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Contrary to popular belief, Lorem Ipsum is not simply random text. "
     const img: ImageDetails[] = [
         { image: '/assets/images/img13.jpg', details: detail },
         { image: '/assets/images/img5.jpg', details: detail },
@@ -76,17 +75,16 @@ export const Home = () => {
     ];
 
     return (
-        <Card sx={{ boxShadow: 'none', borderRadius: 0 }} >
-            <Card sx={{ boxShadow: 'none', borderRadius: 0, display: 'flex' }} >
+        <Card sx={{ boxShadow: 'none', border: "none", p: "2%", }} >
+            <Card sx={{ boxShadow: 'none', border: "none", display: 'flex', gap: "20px" }} >
                 <CardMedia
                     component="img"
-                    sx={{ width: 600 }}
+                    sx={{ height: '400px', width: "55%", borderRadius: "20px" }}
                     image={img[currrIndex].image}
-                    alt="Live from space album cover"
                 />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', width: "42%" }}>
                     <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography component="div" variant="h5">
+                        <Typography component="div" variant="h4">
                             Detailed Info
                         </Typography>
                         <Typography variant="subtitle1" color="text.secondary" component="div" textAlign="left">
@@ -96,15 +94,15 @@ export const Home = () => {
                 </Box>
             </Card>
 
-            <Card sx={{ boxShadow: 'none', borderRadius: 0, display: 'flex' }} >
-                <Card sx={{ boxShadow: 'none', borderRadius: 0 }} >
-                    <IconButton aria-label="play/pause">
+            <Card sx={{ boxShadow: 'none', border: "none", display: 'flex', paddingTop: 3, paddingLeft: 0 }} >
+                <Card sx={{ boxShadow: 'none', borderRadius: 0, display: 'flex', justifyContent: "center", alignItems: "center", }} >
+                    <IconButton aria-label="play/pause" >
                         <NavigateBeforeIcon onClick={handlePrevious} sx={{ height: 40, width: 40 }} />
                     </IconButton>
                     {visibleImages.map((img, ind) => {
                         return (
-                            <img onClick={() => setCurrIndex((currrIndex + ind - 2) % 10)} key={ind} src={img.image} height="100px" style={{
-                                cursor: "pointer", filter: ind === 2 ? 'none' : 'grayscale(100%)',
+                            <img onClick={() => setCurrIndex((currrIndex + ind - 2) % 10)} key={ind} src={img.image} height="80px" width="120px" style={{
+                                cursor: "pointer", filter: ind === 2 ? 'none' : 'grayscale(100%)', borderRadius: "10px", padding: "5px"
                             }} />
                         )
                     })}
@@ -119,7 +117,7 @@ export const Home = () => {
                     </IconButton>
                 </Box>
             </Card>
-        </Card>
+        </Card >
     );
 }
 
